7368fdf217d04e3669e966167f40d664f872a287
VaniaML <vmlvcs@gmail.com>
2023-05-25 13:33:21 -0500

    Footer y Home

--- src/componentes/footer/Footer.jsx
+++ src/componentes/footer/Footer.jsx
@@ -1,6 +1,6 @@
-import style from './Footer.module.css';
 import Face from './face.svg';
 import Whats from './whats.svg';
+import style from './Footer.module.css';
 
 export default function Footer() {
   
@@ -8,7 +8,10 @@ return (
   
   <div className= {style.footer}>
   <div className= {style.image}>
+  <a href="https://www.facebook.com/profile.php?id=100082158084885" target="_blank" rel="noopener noreferrer">
   <img className= {style.img} src={ Face } alt="Face" />
+  <p className= {style.face}>Pro Fitness</p>
+  </a>
   <img className= {style.imgn} src={ Whats } alt="Whats" />
   <p className= {style.num}>+52 553132 4291</p>
   </div>
--- src/componentes/home/Home.jsx
+++ src/componentes/home/Home.jsx
@@ -1,13 +1,4 @@
 import React from 'react';
-import Lau from './lau.jpg';
-import Diamuertos from './diamuertos.jpg';
-import Cross from './cross.jpg';
-import Crossfit from './crossfit.jpg';
-import Neon from './neon.jpg';
-import Step from './step.jpg';
-import Crossf from './crossf.jpg';
-import Crossfi from './crossfi.jpg';
-import Crossft from './crossft.jpg';
 import Gym from './gym.mp4';
 import style from './Home.module.css';
 
@@ -16,17 +7,8 @@ export default function Home() {
 return (
   
   <div className= {style.home}>
-  <img className= {style.img} src={ Lau } alt="Lau" />
-  <img className= {style.img} src={ Diamuertos } alt="Diamuertos" />
-  <img className= {style.img} src={ Cross } alt="Cross" />
-  <img className= {style.img} src={ Crossfit } alt="Crossfit" />
-  <img className= {style.img} src={ Neon } alt="Neon" />
-  <img className= {style.img} src={ Step } alt="Step" />
-  <img className= {style.img} src={ Crossf } alt="Crossf" />
-  <img className= {style.img} src={ Crossfi } alt="Crossfi" />
-  <img className= {style.img} src={ Crossft } alt="Crossft" />
-  <video controls>
-  <source src="./gym.mp4" type="video/mp4" />
+  <video className= {style.video} controls>
+  <source src={Gym} type="video/mp4" />
   </video>
   </div>
 
